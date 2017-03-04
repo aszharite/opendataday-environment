e69a3dd0a4753179104b36d3a469f248fcd712ae
Marales Razvan <r-c.marales@metrosystems.net>
2017-03-04 15:43:28 +0200

    ozone added

--- modules/dashboard/dashboard.controller.js
+++ modules/dashboard/dashboard.controller.js
@@ -42,13 +42,50 @@
                 label: 'very unhealthy',
                 description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
             }, {
-                min: 251,
+                min: 301,
                 max: Number.MAX_VALUE,
                 level: 'red',
                 label: 'hazardous',
                 description: 'Health alert: everyone may experience more serious health effects.'
             }];
-            var O3LevelEnum = [];
+
+            var O3LevelEnum = [{
+                min: 0,
+                max: 50,
+                level: 'green',
+                label: 'good',
+                description: 'Air quality is considered satisfactory, and air pollution poses little or no risk.'
+            }, {
+                min: 51,
+                max: 100,
+                level: 'orange',
+                label: 'moderate',
+                description: 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.'
+            }, {
+                min: 101,
+                max: 150,
+                level: 'orange',
+                label: 'unhealthy for sensitive groups',
+                description: 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.'
+            }, {
+                min: 151,
+                max: 200,
+                level: 'red',
+                label: 'unhealthy',
+                description: 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.'
+            }, {
+                min: 201,
+                max: 300,
+                level: 'red',
+                label: 'very unhealthy',
+                description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
+            }, {
+                min: 301,
+                max: Number.MAX_VALUE,
+                level: 'red',
+                label: 'hazardous',
+                description: 'Health alert: everyone may experience more serious health effects.'
+            }];
             var SO2LevelEnum = [];
             var NO2LevelEnum = [];
 
@@ -57,6 +94,7 @@
             AQILevelEnum.forEach(function(level) {
                 if(level.min <= data.aqi && level.max >= data.aqi) {
                     AQILevel = angular.copy(level);
+                    break;
                 }
             });
 
