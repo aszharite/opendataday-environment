69e5f0680f1ee37d26a3843905b26f3a810fd843
Marales Razvan <r-c.marales@metrosystems.net>
2017-03-04 15:58:49 +0200

    o2 and so2 added

--- modules/dashboard/dashboard.controller.js
+++ modules/dashboard/dashboard.controller.js
@@ -81,13 +81,86 @@
                 description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
             }, {
                 min: 301,
+                max: 500,
+                level: 'red',
+                label: 'hazardous',
+                description: 'Health alert: everyone may experience more serious health effects.'
+            }];
+
+            var NO2LevelEnum = [{
+                min: 0,
+                max: 19,
+                level: 'green',
+                label: 'good',
+                description: 'Air quality is considered satisfactory, and air pollution poses little or no risk.'
+            }, {
+                min: 20,
+                max: 29,
+                level: 'orange',
+                label: 'moderate',
+                description: 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.'
+            }, {
+                min: 30,
+                max: 39,
+                level: 'orange',
+                label: 'unhealthy for sensitive groups',
+                description: 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.'
+            }, {
+                min: 40,
+                max: 49,
+                level: 'red',
+                label: 'unhealthy',
+                description: 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.'
+            }, {
+                min: 50,
+                max: 59,
+                level: 'red',
+                label: 'very unhealthy',
+                description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
+            }, {
+                min: 59,
                 max: Number.MAX_VALUE,
                 level: 'red',
                 label: 'hazardous',
                 description: 'Health alert: everyone may experience more serious health effects.'
             }];
-            var SO2LevelEnum = [];
-            var NO2LevelEnum = [];
+            var SO2LevelEnum = [{
+                min: 0,
+                max: 349,
+                level: 'green',
+                label: 'good',
+                description: 'Air quality is considered satisfactory, and air pollution poses little or no risk.'
+            }, {
+                min: 350,
+                max: 599,
+                level: 'orange',
+                label: 'moderate',
+                description: 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.'
+            }, {
+                min: 600,
+                max: 2599,
+                level: 'orange',
+                label: 'unhealthy for sensitive groups',
+                description: 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.'
+            }, {
+                min: 2600,
+                max: 8999,
+                level: 'red',
+                label: 'unhealthy',
+                description: 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.'
+            }, {
+                min: 9000,
+                max: 13999,
+                level: 'red',
+                label: 'very unhealthy',
+                description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
+            }, {
+                min: 14000,
+                max: Number.MAX_VALUE,
+                level: 'red',
+                label: 'hazardous',
+                description: 'Health alert: everyone may experience more serious health effects.'
+            }];
 
 
             var AQILevel = null;
